253a384e6e3f76d9a762c19cd0c0be0efca0501e
Patrick Heidotting <patrick@lakedigital.nl>
2014-05-05 16:11:52 +0200

    FIX

--- src/test/java/nl/dias/domein/TelefoonnummerTest.java
+++ src/test/java/nl/dias/domein/TelefoonnummerTest.java
@@ -1,78 +1,77 @@
 package nl.dias.domein;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.fail;
 import nl.dias.exception.TelefoonnummerNietGoedException;
 
 import org.junit.Test;
 
 public class TelefoonnummerTest {
 
-	@Test
-	public void validateTeLang() {
-		Telefoonnummer telefoonnummer = new Telefoonnummer();
-		telefoonnummer.setSoort(TelefoonnummerSoort.Mobiel);
-		telefoonnummer.setTelefoonnummer("11111111111");
-		
-		try {
-			telefoonnummer.validate();
-			fail("exception verwacht");
-		} catch (TelefoonnummerNietGoedException e) {
-			//correct
-		}
-	}
+    @Test
+    public void validateTeLang() {
+        Telefoonnummer telefoonnummer = new Telefoonnummer();
+        telefoonnummer.setSoort(TelefoonnummerSoort.MOBIEL);
+        telefoonnummer.setTelefoonnummer("11111111111");
 
-	@Test
-	public void validateTeKort() {
-		Telefoonnummer telefoonnummer = new Telefoonnummer();
-		telefoonnummer.setSoort(TelefoonnummerSoort.Mobiel);
-		telefoonnummer.setTelefoonnummer("111111111");
-		
-		try {
-			telefoonnummer.validate();
-			fail("exception verwacht");
-		} catch (TelefoonnummerNietGoedException e) {
-			//correct
-		}
-	}
-	
-	@Test
-	public void validateAlfanumeriek() {
-		Telefoonnummer telefoonnummer = new Telefoonnummer();
-		telefoonnummer.setSoort(TelefoonnummerSoort.Mobiel);
-		telefoonnummer.setTelefoonnummer("11111a1111");
-		
-		try {
-			telefoonnummer.validate();
-			fail("exception verwacht");
-		} catch (TelefoonnummerNietGoedException e) {
-			//correct
-		}
-	}
-	
-	@Test
-	public void validateLegeSoort() {
-		Telefoonnummer telefoonnummer = new Telefoonnummer();
-		telefoonnummer.setTelefoonnummer("1111111111");
-		
-		try {
-			telefoonnummer.validate();
-			fail("exception verwacht");
-		} catch (TelefoonnummerNietGoedException e) {
-		}
-	}
-	
-	@Test
-	public void validate() {
-		Telefoonnummer telefoonnummer = new Telefoonnummer();
-		telefoonnummer.setSoort(TelefoonnummerSoort.Mobiel);
-		telefoonnummer.setTelefoonnummer("1111111111");
-		
-		try {
-			telefoonnummer.validate();
-		} catch (TelefoonnummerNietGoedException e) {
-			fail("geen exception verwacht");
-		}
-	}
-	
+        try {
+            telefoonnummer.validate();
+            fail("exception verwacht");
+        } catch (TelefoonnummerNietGoedException e) {
+            // correct
+        }
+    }
+
+    @Test
+    public void validateTeKort() {
+        Telefoonnummer telefoonnummer = new Telefoonnummer();
+        telefoonnummer.setSoort(TelefoonnummerSoort.MOBIEL);
+        telefoonnummer.setTelefoonnummer("111111111");
+
+        try {
+            telefoonnummer.validate();
+            fail("exception verwacht");
+        } catch (TelefoonnummerNietGoedException e) {
+            // correct
+        }
+    }
+
+    @Test
+    public void validateAlfanumeriek() {
+        Telefoonnummer telefoonnummer = new Telefoonnummer();
+        telefoonnummer.setSoort(TelefoonnummerSoort.MOBIEL);
+        telefoonnummer.setTelefoonnummer("11111a1111");
+
+        try {
+            telefoonnummer.validate();
+            fail("exception verwacht");
+        } catch (TelefoonnummerNietGoedException e) {
+            // correct
+        }
+    }
+
+    @Test
+    public void validateLegeSoort() {
+        Telefoonnummer telefoonnummer = new Telefoonnummer();
+        telefoonnummer.setTelefoonnummer("1111111111");
+
+        try {
+            telefoonnummer.validate();
+            fail("exception verwacht");
+        } catch (TelefoonnummerNietGoedException e) {
+        }
+    }
+
+    @Test
+    public void validate() {
+        Telefoonnummer telefoonnummer = new Telefoonnummer();
+        telefoonnummer.setSoort(TelefoonnummerSoort.MOBIEL);
+        telefoonnummer.setTelefoonnummer("1111111111");
+
+        try {
+            telefoonnummer.validate();
+        } catch (TelefoonnummerNietGoedException e) {
+            fail("geen exception verwacht");
+        }
+    }
+
 }
