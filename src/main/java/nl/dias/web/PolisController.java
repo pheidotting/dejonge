acf4588e5278d452fdbfe6b8c393f9b25efb0526
Patrick Heidotting <patrick@lakedigital.nl>
2014-05-27 20:04:08 +0200

    sonar

--- src/main/java/nl/dias/web/PolisController.java
+++ src/main/java/nl/dias/web/PolisController.java
@@ -204,8 +204,7 @@ public class PolisController {// extends AbstractController {
             out.flush();
             out.close();
         } catch (IOException e) {
-
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
 
     }
