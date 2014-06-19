9196ce1bb09c1e20eaeee9473b74614279801837
Patrick Heidotting <patrick@lakedigital.nl>
2014-06-19 21:48:52 +0200

    sonar

--- src/main/java/nl/dias/web/BijlageController.java
+++ src/main/java/nl/dias/web/BijlageController.java
@@ -35,11 +35,10 @@ public class BijlageController {
 
     private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
         try {
-            OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
             int read = 0;
             byte[] bytes = new byte[1024];
 
-            out = new FileOutputStream(new File(uploadedFileLocation));
+            OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
             while ((read = uploadedInputStream.read(bytes)) != -1) {
                 out.write(bytes, 0, read);
             }
