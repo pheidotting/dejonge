522b3bc5b929882b1b7a0cdf9b259b5fa0251e5a
Patrick Heidotting <patrick@lakedigital.nl>
2014-07-09 20:33:26 +0200

    sonar

--- src/test/java/nl/dias/dias_web/medewerker/KantoorController.java
+++ src/test/java/nl/dias/dias_web/medewerker/KantoorController.java
@@ -1,51 +1,48 @@
 package nl.dias.dias_web.medewerker;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 import com.google.gson.Gson;
 
 @Path("/kantoor")
 public class KantoorController {// implements KantoorControllerInterface {
-    private Gson gson = new Gson();
+    private final Gson gson = new Gson();
 
-    // @GET
-    // @Path("/aanmelden")
-    // @Produces(MediaType.TEXT_PLAIN)
-    // public String aanmelden(@QueryParam("identificatie") String
-    // identificatie, @QueryParam("waar") String waar, @Context
-    // HttpServletRequest request) {
-    // // TODO Auto-generated method stub
-    // return "A";
-    // }
-    //
-    // public String leesKantoor(@Context HttpServletRequest request) {
-    // return null;
-    // }
+    @GET
+    @Path("/aanmelden")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String aanmelden(@QueryParam("identificatie") String identificatie, @QueryParam("waar") String waar, @Context HttpServletRequest request) {
+        return "A";
+    }
+
+    public String leesKantoor(@Context HttpServletRequest request) {
+        return null;
+    }
 
     @GET
     @Path("/lees")
-    // @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
     public String leesKantoor() {
-        System.out.println("SAFDsdfsdf");
         return gson.toJson("B");
     }
 
-    // @GET
-    // @Path("/opslaan")
-    // @Produces(MediaType.TEXT_PLAIN)
-    // public String opslaan(@QueryParam("kantoor") String strKantoor, @Context
-    // HttpServletRequest request) {
-    // // TODO Auto-generated method stub
-    // return "C";
-    // }
-    //
-    // @GET
-    // @Path("/verwijder")
-    // @Produces(MediaType.TEXT_PLAIN)
-    // public String verwijder(@QueryParam("id") String id, @Context
-    // HttpServletRequest request) {
-    // // TODO Auto-generated method stub
-    // return "D";
-    // }
+    @GET
+    @Path("/opslaan")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String opslaan(@QueryParam("kantoor") String strKantoor, @Context HttpServletRequest request) {
+        return "C";
+    }
+
+    @GET
+    @Path("/verwijder")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String verwijder(@QueryParam("id") String id, @Context HttpServletRequest request) {
+        return "D";
+    }
 }
