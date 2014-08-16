3c8f04c54d1491bcd9e3cf611de53d06c8a635b3
Patrick Heidotting <patrick@lakedigital.nl>
2014-08-16 09:48:21 +0200

    sonar

--- WebContent/pages/beherenRelatie/details/schades.js
+++ WebContent/pages/beherenRelatie/details/schades.js
@@ -7,7 +7,7 @@ function go(log, relatieId, actie, subId){
 
 function Schade(data, log, relatieId){
 	var self = this;
-	
+
     self.id = ko.observable(data.id);
     self.polis = ko.observable(data.polis);
     self.schadeNummerMaatschappij = ko.observable(data.schadeNummerMaatschappij);
@@ -20,7 +20,7 @@ function Schade(data, log, relatieId){
     self.datumAfgehandeld = ko.observable(data.datumAfgehandeld);
     self.eigenRisico = ko.observable(data.eigenRisico);
     self.omschrijving = ko.observable(data.omschrijving);
-    
+
 	self.idDiv = ko.computed(function() {
         return "collapsableSchade" + data.id;
 	}, this);
@@ -54,22 +54,21 @@ function Schade(data, log, relatieId){
     				uploadBestand(formData, '../dejonge/rest/medewerker/bijlage/uploadSchade' + int + 'File');
     			}
             	plaatsMelding("De gegevens zijn opgeslagen");
-//				$('#tabs').puitabview('select', 5);
             },
             error: function (data) {
             	plaatsFoutmelding(data);
             }
         });
-	}
-    	
+	};
+
     self.bewerkSchade = function(schade){
     	document.location.hash = "#beherenRelatie/" + relatieId + "/schade/" + ko.utils.unwrapObservable(schade.id);
-    }
+    };
 }
 
 function Schades(data, log, relatieId){
 	var self = this;
-	
+
 	self.schades = ko.observableArray();
 	$.each(data, function(i, item){
 		self.schades.push(new Schade(item, log, relatieId));
@@ -81,5 +80,5 @@ function Schades(data, log, relatieId){
 			self.schades.remove(schade);
 			$.get( "../dejonge/rest/medewerker/schade/verwijder", {"id" : ko.utils.unwrapObservable(schade.id)}, function( data ) {});
 		}
-    }
+    };
 }
