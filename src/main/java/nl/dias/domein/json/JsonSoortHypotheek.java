ddad329f583803b0b2de66c2858173269da36e0f
Patrick Heidotting <patrick@lakedigital.nl>
2014-11-03 20:57:17 +0100

    sonar

--- src/main/java/nl/dias/domein/json/JsonSoortHypotheek.java
+++ src/main/java/nl/dias/domein/json/JsonSoortHypotheek.java
@@ -30,6 +30,48 @@ public class JsonSoortHypotheek implements Comparable<JsonSoortHypotheek> {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + (ingebruik ? 1231 : 1237);
+        result = prime * result + ((omschrijving == null) ? 0 : omschrijving.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        JsonSoortHypotheek other = (JsonSoortHypotheek) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (ingebruik != other.ingebruik) {
+            return false;
+        }
+        if (omschrijving == null) {
+            if (other.omschrijving != null) {
+                return false;
+            }
+        } else if (!omschrijving.equals(other.omschrijving)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public int compareTo(JsonSoortHypotheek o) {
         return omschrijving.compareTo(o.getOmschrijving());
     }
