ce63f8a7faa3c0efe77ce367df18d5c1dcfef608
Patrick Heidotting <patrick@lakedigital.nl>
2014-11-10 18:35:29 +0100

    dekking++;

--- src/test/java/nl/dias/service/BijlageServiceTest.java
+++ src/test/java/nl/dias/service/BijlageServiceTest.java
@@ -1,5 +1,14 @@
 package nl.dias.service;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.dias.domein.Bijlage;
+import nl.dias.domein.Relatie;
 import nl.dias.repository.BijlageRepository;
 import nl.lakedigital.archief.service.ArchiefService;
 
@@ -26,19 +35,39 @@ public class BijlageServiceTest extends EasyMockSupport {
 
     @After
     public void tearDown() throws Exception {
-        // verifyAll();
+        verifyAll();
     }
 
     @Test
     public void testAlleBijlagesBijRelatie() {
+        Relatie relatie = createMock(Relatie.class);
+
+        List<Bijlage> bijlages = new ArrayList<Bijlage>();
+
+        expect(repository.alleBijlagesBijRelatie(relatie)).andReturn(bijlages);
+
+        replayAll();
+
+        assertEquals(bijlages, service.alleBijlagesBijRelatie(relatie));
     }
 
     @Test
-    public void testUploaden() {
-    }
+    public void testVerwijderBijlage() {
+        Bijlage bijlage = createMock(Bijlage.class);
+        expect(repository.lees(3L)).andReturn(bijlage);
 
-    @Test
-    public void testWriteToFile() {
-    }
+        archiefService.setBucketName("dias");
+        expectLastCall();
 
+        expect(bijlage.getS3Identificatie()).andReturn("1234");
+        archiefService.verwijderen("1234");
+        expectLastCall();
+
+        repository.verwijder(bijlage);
+        expectLastCall();
+
+        replayAll();
+
+        service.verwijderBijlage(3L);
+    }
 }
