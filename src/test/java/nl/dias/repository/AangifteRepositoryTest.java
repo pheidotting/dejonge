83c66861a178292b557b12d2ee6ed5f1925a8803
Patrick Heidotting <patrick@lakedigital.nl>
2015-02-06 18:26:35 +0100

    ff uit

--- src/test/java/nl/dias/repository/AangifteRepositoryTest.java
+++ src/test/java/nl/dias/repository/AangifteRepositoryTest.java
@@ -1,58 +1,58 @@
-package nl.dias.repository;
-
-import static org.junit.Assert.assertEquals;
-import nl.dias.domein.Aangifte;
-import nl.dias.domein.Medewerker;
-import nl.dias.domein.Relatie;
-
-import org.joda.time.LocalDate;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-@Ignore
-public class AangifteRepositoryTest {
-    private AangifteRepository aangifteRepository;
-
-    @Before
-    public void setUp() throws Exception {
-        aangifteRepository = new AangifteRepository();
-        aangifteRepository.zetPersistenceContext("unittest");
-    }
-
-    @Test
-    public void testGetOpenAngiftes() {
-        Medewerker medewerker = new Medewerker();
-
-        Relatie relatie = new Relatie();
-
-        aangifteRepository.getEm().getTransaction().begin();
-        aangifteRepository.getEm().persist(medewerker);
-        aangifteRepository.getEm().persist(relatie);
-        aangifteRepository.getEm().getTransaction().commit();
-
-        Aangifte aangifte = new Aangifte();
-        aangifte.setDatumAfgerond(LocalDate.now());
-        aangifte.setJaar(2013);
-        aangifte.setAfgerondDoor(medewerker);
-        aangifte.setRelatie(relatie);
-
-        aangifteRepository.opslaan(aangifte);
-
-        assertEquals(1, aangifteRepository.alles().size());
-        assertEquals(0, aangifteRepository.getOpenAngiftes(relatie).size());
-
-        aangifte.reset();
-        aangifteRepository.opslaan(aangifte);
-
-        assertEquals(1, aangifteRepository.alles().size());
-        assertEquals(1, aangifteRepository.getOpenAngiftes(relatie).size());
-
-        aangifte.afhandelen(medewerker);
-        aangifteRepository.opslaan(aangifte);
-
-        assertEquals(1, aangifteRepository.alles().size());
-        assertEquals(0, aangifteRepository.getOpenAngiftes(relatie).size());
-    }
-
-}
+//package nl.dias.repository;
+//
+//import static org.junit.Assert.assertEquals;
+//import nl.dias.domein.Aangifte;
+//import nl.dias.domein.Medewerker;
+//import nl.dias.domein.Relatie;
+//
+//import org.joda.time.LocalDate;
+//import org.junit.Before;
+//import org.junit.Ignore;
+//import org.junit.Test;
+//
+//@Ignore
+//public class AangifteRepositoryTest {
+//    private AangifteRepository aangifteRepository;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        aangifteRepository = new AangifteRepository();
+//        aangifteRepository.zetPersistenceContext("unittest");
+//    }
+//
+//    @Test
+//    public void testGetOpenAngiftes() {
+//        Medewerker medewerker = new Medewerker();
+//
+//        Relatie relatie = new Relatie();
+//
+//        aangifteRepository.getEm().getTransaction().begin();
+//        aangifteRepository.getEm().persist(medewerker);
+//        aangifteRepository.getEm().persist(relatie);
+//        aangifteRepository.getEm().getTransaction().commit();
+//
+//        Aangifte aangifte = new Aangifte();
+//        aangifte.setDatumAfgerond(LocalDate.now());
+//        aangifte.setJaar(2013);
+//        aangifte.setAfgerondDoor(medewerker);
+//        aangifte.setRelatie(relatie);
+//
+//        aangifteRepository.opslaan(aangifte);
+//
+//        assertEquals(1, aangifteRepository.alles().size());
+//        assertEquals(0, aangifteRepository.getOpenAngiftes(relatie).size());
+//
+//        aangifte.reset();
+//        aangifteRepository.opslaan(aangifte);
+//
+//        assertEquals(1, aangifteRepository.alles().size());
+//        assertEquals(1, aangifteRepository.getOpenAngiftes(relatie).size());
+//
+//        aangifte.afhandelen(medewerker);
+//        aangifteRepository.opslaan(aangifte);
+//
+//        assertEquals(1, aangifteRepository.alles().size());
+//        assertEquals(0, aangifteRepository.getOpenAngiftes(relatie).size());
+//    }
+//
+// }
