88bcbd23f223299822181790b9665ad263e17b86
Patrick Heidotting <patrick@lakedigital.nl>
2015-04-11 16:36:09 +0200

    just trying

--- src/main/java/nl/dias/repository/GebruikerRepository.java
+++ src/main/java/nl/dias/repository/GebruikerRepository.java
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public class GebruikerRepository extends AbstractRepository<Gebruiker> {
     private final static Logger LOGGER = Logger.getLogger(GebruikerRepository.class);
-    private final static int MAX_RESULTS = 25;
+    private final static int MAX_RESULTS = 10;
 
     public GebruikerRepository() {
         super(Gebruiker.class);
