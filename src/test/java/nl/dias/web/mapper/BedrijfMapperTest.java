0b353fe58acb64b522bfc8635afc696b8eea306d
Patrick Heidotting <patrick@lakedigital.nl>
2015-10-26 20:03:49 +0100

    DJFC-2

--- src/test/java/nl/dias/web/mapper/BedrijfMapperTest.java
+++ src/test/java/nl/dias/web/mapper/BedrijfMapperTest.java
@@ -4,8 +4,13 @@ import nl.dias.domein.Adres;
 import nl.dias.domein.Bedrijf;
 import nl.dias.domein.Relatie;
 import nl.dias.domein.json.JsonBedrijf;
+import org.easymock.EasyMockRunner;
+import org.easymock.EasyMockSupport;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -13,18 +18,20 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-
-public class BedrijfMapperTest {
-    private BedrijfMapper mapper;
+@RunWith(EasyMockRunner.class)
+public class BedrijfMapperTest extends EasyMockSupport{
+@TestSubject
+    private BedrijfMapper mapper= new BedrijfMapper();
     private Bedrijf bedrijf;
     private JsonBedrijf jsonBedrijf;
     private Set<Bedrijf> bedrijven;
     private List<JsonBedrijf> jsonBedrijven;
 
+    @Mock
+    private OpmerkingMapper opmerkingMapper;
+
     @Before
     public void setUp() throws Exception {
-        mapper = new BedrijfMapper();
-
         bedrijf = new Bedrijf();
         Adres adres = new Adres();
         adres.setHuisnummer(41L);
