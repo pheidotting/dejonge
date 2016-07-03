3fc1d80c15352f2a845f060abadd8dff597b20ff
Patrick Heidotting <patrick@lakedigital.nl>
2016-07-03 21:06:42 +0200

    DJFC-13

--- src/test/java/nl/dias/it/PolisOpslaanTest.java
+++ src/test/java/nl/dias/it/PolisOpslaanTest.java
@@ -1,17 +1,21 @@
 package nl.dias.it;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import nl.dias.domein.Bedrag;
 import nl.dias.domein.Medewerker;
 import nl.dias.domein.Sessie;
 import nl.dias.domein.StatusPolis;
 import nl.dias.domein.polis.Betaalfrequentie;
 import nl.dias.domein.polis.Polis;
+import nl.dias.domein.polis.SoortVerzekering;
 import nl.dias.repository.GebruikerRepository;
 import nl.dias.web.mapper.PolisMapper;
 import nl.lakedigital.djfc.client.polisadministratie.PolisClient;
 import nl.lakedigital.djfc.commons.json.JsonPolis;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -20,8 +24,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext-it.xml")
@@ -39,7 +50,7 @@ public class PolisOpslaanTest {
     private GebruikerRepository gebruikerRepository;
 
     @Test
-    public void test() {
+    public void opslaanAllePolissen() {
         Medewerker medewerker = (Medewerker) gebruikerRepository.lees(3L);
         Sessie sessie = new Sessie();
         sessie.setSessie("abc");
@@ -91,5 +102,39 @@ public class PolisOpslaanTest {
         }
 
         return stringBuilder.toString().substring(0, lengte);
+    }
+
+    @Test
+    @Ignore
+    public void testLijstSoortenPolissen() {
+        List<String> particulier = filterPolissenOpSoort(SoortVerzekering.PARTICULIER);
+        List<String> zakelijk = filterPolissenOpSoort(SoortVerzekering.ZAKELIJK);
+
+        Collections.sort(particulier);
+        Collections.sort(zakelijk);
+
+        List<String> particulierOpgehaald = polisClient.alleParticulierePolisSoorten();
+        List<String> zakelijkOpgehaald = polisClient.alleZakelijkePolisSoorten();
+
+        Collections.sort(particulierOpgehaald);
+        Collections.sort(zakelijkOpgehaald);
+
+        assertThat(particulierOpgehaald, is(particulier));
+        assertThat(zakelijkOpgehaald, is(zakelijk));
+    }
+
+
+    private List<String> filterPolissenOpSoort(final SoortVerzekering soortVerzekering) {
+        return newArrayList(transform(filter(polissen, new Predicate<Polis>() {
+            @Override
+            public boolean apply(Polis polis) {
+                return polis.getSoortVerzekering().equals(soortVerzekering);
+            }
+        }), new Function<Polis, String>() {
+            @Override
+            public String apply(Polis polis) {
+                return polis.getSchermNaam();
+            }
+        }));
     }
 }
