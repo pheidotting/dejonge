a0856970599f9abf930a6512c8c0baa9278f3b83
Patrick Heidotting <patrick@lakedigital.nl>
2017-01-22 14:59:42 +0100

    sonar

--- src/main/java/nl/dias/service/PostcodeService.java
+++ src/main/java/nl/dias/service/PostcodeService.java
@@ -12,13 +12,13 @@ public class PostcodeService {
         int index = apiAntwoord.indexOf("label", apiAntwoord.indexOf("city"));
         if (index > -1) {
             String plaats = apiAntwoord.substring(index + 8);
-                index = plaats.indexOf("\"");
+            index = plaats.indexOf('\"');
                 if (index > -1) {
                     plaats = plaats.substring(0, index);
                     jsonAdres.setPlaats(plaats.toUpperCase());
 
                     String straat = apiAntwoord.substring(apiAntwoord.indexOf("street") + 9);
-                    index = straat.indexOf("\"");
+                    index = straat.indexOf('\"');
                     if (index > -1) {
                         straat = straat.substring(0, index);
                         straat = straat.substring(0, 1).toUpperCase() + straat.substring(1).toLowerCase();
