fcf06916b5d32efec674504493c5c524e30e9e15
Patrick Heidotting <patrick@lakedigital.nl>
2017-02-03 22:00:39 +0100

    sonar

--- src/main/java/nl/dias/service/WachtwoordService.java
+++ src/main/java/nl/dias/service/WachtwoordService.java
@@ -1,6 +1,8 @@
 package nl.dias.service;
 
 import nl.dias.domein.Gebruiker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
@@ -8,14 +10,14 @@ import java.security.NoSuchAlgorithmException;
 
 @Service
 public class WachtwoordService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WachtwoordService.class);
+
     public String hashWachtwoord(String wachtwoord, Gebruiker gebruiker) {
         String ww = null;
         try {
             ww = wachtwoord + gebruiker.getSalt();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            LOGGER.error("{}", e);
         }
 
         return ww;
