252328ef3fd37b01193e76dbde716eb65dd9bca9
Patrick Heidotting <patrick@lakedigital.nl>
2017-10-21 17:36:11 +0200

    Opslaan Polissen bij Bedrijf

--- src/main/java/nl/dias/service/BedrijfService.java
+++ src/main/java/nl/dias/service/BedrijfService.java
@@ -9,6 +9,14 @@ import nl.dias.messaging.sender.VerwijderEntiteitenRequestSender;
 import nl.dias.repository.BedrijfRepository;
 import nl.dias.repository.PolisRepository;
 import nl.lakedigital.as.messaging.domain.SoortEntiteit;
+import nl.lakedigital.djfc.client.identificatie.IdentificatieClient;
+import nl.lakedigital.djfc.client.oga.AdresClient;
+import nl.lakedigital.djfc.client.polisadministratie.PolisClient;
+import nl.lakedigital.djfc.client.polisadministratie.SchadeClient;
+import nl.lakedigital.djfc.commons.json.Identificatie;
+import nl.lakedigital.djfc.commons.json.JsonAdres;
+import nl.lakedigital.djfc.commons.json.JsonPolis;
+import nl.lakedigital.djfc.commons.json.JsonSchade;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
@@ -30,6 +38,14 @@ public class BedrijfService {
     private SchadeService schadeService;
     @Inject
     private PolisRepository polisRepository;
+    @Inject
+    private IdentificatieClient identificatieClient;
+    @Inject
+    private PolisClient polisClient;
+    @Inject
+    private AdresClient adresClient;
+    @Inject
+    private SchadeClient schadeClient;
 
     public void opslaan(Bedrijf bedrijf) {
         bedrijfRepository.opslaan(bedrijf);
@@ -37,6 +53,51 @@ public class BedrijfService {
 
     public Bedrijf lees(Long id) {
         return bedrijfRepository.lees(id);
+    }
+
+    public Bedrijf zoekBedrijf(String identificatieCode) {
+        Identificatie identificatie = identificatieClient.zoekIdentificatieCode(identificatieCode);
+
+        Long bedrijfId = null;
+
+        switch (identificatie.getSoortEntiteit()) {
+            case "RELATIE":
+                bedrijfId = identificatie.getEntiteitId();
+                break;
+            case "POLIS":
+                bedrijfId = pakBedrijfBijPolis(identificatie.getEntiteitId());
+                break;
+            case "ADRES":
+                bedrijfId = pakBedrijfBijAdres(identificatie.getEntiteitId());
+                break;
+            case "SCHADE":
+                bedrijfId = pakBedrijfBijSchade(identificatie.getEntiteitId());
+                break;
+        }
+
+        return (Bedrijf) bedrijfRepository.lees(bedrijfId);
+    }
+
+    private Long pakBedrijfBijPolis(Long polisId) {
+        JsonPolis polis = polisClient.lees(String.valueOf(polisId));
+
+        LOGGER.debug("Polis ({}) gevonden : {}", polisId, ReflectionToStringBuilder.toString(polis));
+
+        return polis.getEntiteitId();
+    }
+
+    private Long pakBedrijfBijAdres(Long adresId) {
+        JsonAdres adres = adresClient.lees(adresId);
+
+        return adres.getEntiteitId();
+    }
+
+    private Long pakBedrijfBijSchade(Long schadeId) {
+        JsonSchade schade = schadeClient.lees(String.valueOf(schadeId));
+
+        LOGGER.debug("Schade ({}) gevonden : {}", schadeId, ReflectionToStringBuilder.toString(schade));
+
+        return pakBedrijfBijPolis(Long.valueOf(schade.getPolis()));
     }
 
     public void verwijder(Long id) {
