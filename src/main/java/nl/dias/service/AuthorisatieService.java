576726159853d519469ab09af769dc588b8fb474
Patrick Heidotting <patrick@lakedigital.nl>
2017-10-22 17:37:20 +0200

    beter zo

--- src/main/java/nl/dias/service/AuthorisatieService.java
+++ src/main/java/nl/dias/service/AuthorisatieService.java
@@ -73,16 +73,13 @@ public class AuthorisatieService {
         if (!uitZabbix) {
             LOGGER.debug("Aanmaken nieuwe sessie");
         }
+        String nieuweSessie = UUID.randomUUID().toString();
         Sessie sessie = new Sessie();
         sessie.setBrowser(request.getHeader("user-agent"));
         sessie.setIpadres(request.getRemoteAddr());
         sessie.setDatumLaatstGebruikt(new Date());
         sessie.setGebruiker(gebruikerUitDatabase);
-        sessie.setSessie(UUID.randomUUID().toString());
-
-        if (!uitZabbix) {
-            LOGGER.debug("Nieuwe sessie : {}", sessie.getSessie());
-        }
+        sessie.setSessie(nieuweSessie);
 
         gebruikerService.opslaan(sessie);
 
@@ -91,11 +88,11 @@ public class AuthorisatieService {
         gebruikerService.opslaan(gebruikerUitDatabase);
 
         if (!uitZabbix) {
-            LOGGER.debug("sessie id " + sessie.getSessie() + " in de request plaatsen");
+            LOGGER.debug("sessie id {} in de request en response plaatsen", nieuweSessie);
         }
-        request.getSession().setAttribute("sessie", sessie.getSessie());
+        request.getSession().setAttribute("sessie", nieuweSessie);
 
-        response.setHeader("sessie", sessie.getSessie());
+        response.setHeader("sessie", nieuweSessie);
     }
 
     public Gebruiker getIngelogdeGebruiker(HttpServletRequest request, String sessieId, String ipadres) {
